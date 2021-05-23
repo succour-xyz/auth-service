1b0acb80d0b574cffbd7d3c6970781e415926d06
Piyush Mehta <me@piyushmehta.com>
2021-05-23 15:08:23 +0530

    apollo server setup

---
+++ api-gateway/src/config/default.ts
@@ -0,0 +1,3 @@
+const PORT = 7000;
+
+export default { PORT };
--- api-gateway/src/server/startServer.ts
+++ api-gateway/src/server/startServer.ts
@@ -1,10 +1,34 @@
+import { ApolloServer } from "apollo-server-express";
+import formatGraphQLErrors from "../helpers/formatGraphQLErrors";
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import resolvers from "../graphql/resolvers";
+import config from "../config/default";
+import schema from "../graphql/schema";
 
 const startServer = () => {
-  const app = express();
+  const apolloServer = new ApolloServer({
+    context: (a) => a,
+    formatError: formatGraphQLErrors,
+    resolvers,
+    typeDefs: schema,
+  });
 
-  app.listen(7000, "0.0.0.0", () => {
-    console.info("API gateway listening on 7000");
+  const app = express();
+  app.use(cookieParser());
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: (origin, cb) => cb(null, true),
+    })
+  );
+
+  apolloServer.applyMiddleware({ app, cors: false, path: "/graphql" });
+
+  app.listen(config.PORT, "0.0.0.0", () => {
+    console.info(`API gateway listening on ${config.PORT}`);
   });
 };
 
