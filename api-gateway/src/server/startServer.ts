79ad3e642a7b758ecae5a34753d6bbff563f26ef
Piyush Mehta <me@piyushmehta.com>
2021-05-23 14:37:32 +0530

    server

--- api-gateway/src/server/startServer.ts
+++ api-gateway/src/server/startServer.ts
@@ -1,5 +1,11 @@
+import express from "express";
+
 const startServer = () => {
-  return "WORKING";
+  const app = express();
+
+  app.listen(7000, "0.0.0.0", () => {
+    console.info("API gateway listening on 7000");
+  });
 };
 
 export default startServer;
