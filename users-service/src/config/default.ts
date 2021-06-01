c6ef5bc7b355bc4922fc475e7acc4995192435d0
Piyush Mehta <me@piyushmehta.com>
2021-06-01 20:30:26 +0530

    revamp

--- users-service/src/config/default.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2021.  Piyush Mehta for Succour.xyz
- */
-
-const USERS_SERVICE_DB_URL = "mysql://root:password@users-service-db/db";
-const USER_SESSION_EXPIRY_HOURS = 1;
-
-export default { USER_SESSION_EXPIRY_HOURS, USERS_SERVICE_DB_URL };
