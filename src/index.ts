59a651b22e14914e8c6ac6f80a5ed2fa4dc548d2
Piyush Mehta <me@piyushmehta.com>
2021-06-01 22:48:01 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
-import PORT from "./constants/appConstants";
 import UserRoutes from "./routes/user";
+import PORT from "./util/secrets";
 
 const app = express();
 
 // app.use(bodyParser.json());
 
 app.use(UserRoutes);
+
 app.listen(PORT, () => {
   console.info("Server running on port:", PORT);
 });
