ec3bd78b97bd1dfe3c53b18e8dbf83d2ca455229
Piyush Mehta <me@piyushmehta.com>
2021-06-03 18:18:25 +0530

    login

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -10,3 +10,8 @@ export const PASSWORD_MISMATCH = "Passwords Don't match";
  * Constant Email Not Found
  */
 export const EMAIL_NOT_FOUND = "Email Not Registered, Please Sign Up";
+
+/**
+ * Constant Enter correct Password
+ */
+export const ENTER_CORRECT_PASSWORD = "Please enter correct password";
--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,12 +1,18 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
+import { error } from "winston";
 import {
   EMAIL_DUPLICATE,
   EMAIL_NOT_FOUND,
   PASSWORD_MISMATCH,
 } from "../constants/messages";
 import prisma from "../util/db";
+import {
+  ENCRYPTION_COMPARE_FAIL,
+  ENCRYPTION_FAIL,
+} from "./../constants/errors";
+import { ENTER_CORRECT_PASSWORD } from "./../constants/messages";
 import { SignUpBody } from "./../types/User/index";
 
 export default class Auth {
@@ -20,25 +26,30 @@ export default class Auth {
     }
     try {
       const hashedPassword = await bcrypt.hash(password, 10);
-      bcrypt.compare(confirmPassword, hashedPassword).then(async (same) => {
-        if (same) {
-          try {
-            const result = await prisma.user.create({
-              data: {
-                email,
-                password: hashedPassword,
-                name,
-              },
-            });
-            return res.sendStatus(201).json(result);
-          } catch (error) {
-            res.json({ message: EMAIL_DUPLICATE });
-            return res.status(409).end();
+      bcrypt
+        .compare(confirmPassword, hashedPassword)
+        .then(async (same) => {
+          if (same) {
+            try {
+              const result = await prisma.user.create({
+                data: {
+                  email,
+                  password: hashedPassword,
+                  name,
+                },
+              });
+              return res.sendStatus(201).json(result);
+            } catch (error) {
+              res.json({ message: EMAIL_DUPLICATE });
+              return res.status(409).end();
+            }
+          } else {
+            return res.sendStatus(406).json({ message: PASSWORD_MISMATCH });
           }
-        } else {
-          return res.sendStatus(406).json({ message: PASSWORD_MISMATCH });
-        }
-      });
+        })
+        .catch((error) => {
+          return console.error(ENCRYPTION_FAIL, error);
+        });
     } catch (error) {
       return res.sendStatus(406).json({ message: "Error" });
     }
@@ -46,13 +57,25 @@ export default class Auth {
 
   static login = async (req: Request, res: Response): Promise<unknown> => {
     const body = req.body as SignUpBody;
-    const email = body.email;
+    const { email, password } = body;
     try {
       await prisma.user
         .findUnique({ where: { email } })
         .then((result) => {
           if (result) {
-            res.sendStatus(200).json(result);
+            bcrypt
+              .compare(password, result.password)
+              .then((doMatch) => {
+                if (doMatch) {
+                  res.sendStatus(200);
+                } else {
+                  res.statusCode = 406;
+                  res.json({ message: ENTER_CORRECT_PASSWORD, error }).end();
+                }
+              })
+              .catch((error) => {
+                return console.error(ENCRYPTION_COMPARE_FAIL, error);
+              });
           } else {
             return res.status(404).json({ message: EMAIL_NOT_FOUND }).end();
           }
